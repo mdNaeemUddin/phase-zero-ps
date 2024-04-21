00f92380271e4f7cc8c882a023614195feac09cb
mdNaeemUddin <md.naeem_uddin98@yahoo.com>
2024-04-21 22:42:17 +0600

    learn C++ with shohag vhai : phase-zero : problem-solving start: lesson - zero

---
+++ phase-zero-lesoon-zero.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int sumFunc(int a, int b){
+    return a + b;
+}
+
+
+int main()
+{
+    int a, b; cin >> a >> b;
+
+    cout << "sum is : " << sumFunc(a, b)  << endl;
+    return 0;
+}
